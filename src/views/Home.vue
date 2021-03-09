155b76ffb1bfee741fb2b3fa82b6c77d2593bcb9
seongpil0948 <seongpil0948@gmail.com>
2021-03-09 12:44:48 +0900

    for test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -70,7 +70,7 @@ export default {
     get_code() {
       let url = `https://${this.shopId}.cafe24api.com/api/v2/oauth/authorize`
       alert(this.makeParam(url, this.authCodeParams))
-      window.location.href = this.makeParam(url, this.authCodeParams)
+      // window.location.href = this.makeParam(url, this.authCodeParams)
     },
     get_token({ code }) {
       const url = 'https://tjagksro.cafe24api.com/api/v2/oauth/token'
